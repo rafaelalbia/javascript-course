2cdad90518bd66038ef8f681ec55d6d49ad11b73
rafaelalbia <rafaelalbia@outlook.com>
2021-01-26 15:19:56 -0300

    Exercises import

---
+++ exercises/exercise03/exercise03.js
@@ -0,0 +1,11 @@
+let person = {
+    name: "Rafael",
+    age: 19,
+    situation: true,
+    cover: undefined,
+    color: null
+};
+
+let ages = [17, 19, 59, 61];
+
+console.log(person, ages);
---
+++ lesson.js
@@ -0,0 +1,13 @@
+let name = "Rafael";
+let age = 19;
+let situation = true;
+let cover = undefined;
+let color = null;
+
+let person = {
+    name: "Rafael",
+    age: "19",
+    situation: true,
+    cover: undefined,
+    color: null
+};
