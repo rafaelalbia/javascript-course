ebb78d2dd0da62bc2c7add587913963fbe0ffa65
rafaelalbia <rafaelalbia@outlook.com>
2021-01-26 15:01:20 -0300

    Exercises import

---
+++ exercises/exercise02/exercise02.js
@@ -0,0 +1,6 @@
+let name = "Rafael"; // String
+let age = 19; // Number literal
+let situation = true; // Boolean
+let cover = undefined; // Undefined
+let color = null; // Reset a value
+console.log(name, age, situation, cover, color);
