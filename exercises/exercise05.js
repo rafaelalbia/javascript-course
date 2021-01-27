596f41a752fbbd96b1c7dd5797596dab54c9fc56
rafaelalbia <rafaelalbia@outlook.com>
2021-01-27 11:55:22 -0300

    Exercises import

--- exercises/exercise05.js
+++ exercises/exercise05.js
@@ -9,3 +9,27 @@ console.log(number / number);
 console.log(number ** number);
 console.log(++number);
 console.log(--number)
+
+// Assignment operators
+let priceKeyboard = 100;
+
+console.log(priceKeyboard);
+priceKeyboard += priceKeyboard;
+console.log(priceKeyboard);
+priceKeyboard -= priceKeyboard;
+console.log(priceKeyboard);
+priceKeyboard *= priceKeyboard;
+console.log(priceKeyboard);
+priceKeyboard /= priceKeyboard;
+console.log(priceKeyboard);
+
+// Equality operators
+console.log(1 === 1);
+console.log("1" === 1);
+console.log("1" == 1);
+
+// Ternary operators
+let costumer = 100;
+let type = costumer >= 100 ? "premium": "common";
+
+console.log(type);
