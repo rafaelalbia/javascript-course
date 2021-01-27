3ff1e27818f294ebe64805dbe52db1fcbd4edc5f
rafaelalbia <rafaelalbia@outlook.com>
2021-01-27 11:47:37 -0300

    Exercises import

---
+++ exercises/exercise06.js
@@ -0,0 +1,12 @@
+// Assignment operators
+let priceKeyboard = 100;
+
+console.log(priceKeyboard);
+priceKeyboard += priceKeyboard;
+console.log(priceKeyboard);
+priceKeyboard -= priceKeyboard;
+console.log(priceKeyboard);
+priceKeyboard *= priceKeyboard;
+console.log(priceKeyboard);
+priceKeyboard /= priceKeyboard;
+console.log(priceKeyboard);
