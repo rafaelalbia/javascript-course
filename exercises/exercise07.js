e045996f307103edbf25c245726bd6733eb40b2b
rafaelalbia <rafaelalbia@outlook.com>
2021-01-27 14:18:22 -0300

    Exercises import

---
+++ exercises/exercise07.js
@@ -0,0 +1,28 @@
+// If-else
+let hour = 1;
+
+if (hour > 6 && hour < 12) {
+    console.log("Good morning");
+} else if (hour > 12 && hour < 18) {
+    console.log("Good afternoon")
+} else {
+    console.log("Good night");
+}
+
+// Switch-case
+let choice;
+
+switch (choice) {
+    case "one":
+        console.log(1);
+        break;
+    case "two":
+        console.log(2);
+        break;
+    case "three":
+        console.log(3);
+        break;
+    default:
+        console.log(4);
+        break;
+}
