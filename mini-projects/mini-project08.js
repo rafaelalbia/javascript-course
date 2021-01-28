0b5b8d3a4166b42042e2f00d8c40a586fdb823b9
rafaelalbia <rafaelalbia@outlook.com>
2021-01-28 12:20:04 -0300

    Mini-Project08 import

--- mini-projects/mini-project08.js
+++ mini-projects/mini-project08.js
@@ -3,28 +3,24 @@ const array = [70, 70, 80];
 console.log(studentAverage(array));
 
 function studentAverage(grades) {
+    let result = average(grades);
+
+    if (result < 0) return "Invalid"
+    if (result < 59) return "F";
+    if (result < 69) return "D";
+    if (result < 79) return "C";
+    if (result < 89) return "B";
+    return "A";
+}
+
+function average(array) {
     let result = 0;
     let index = 0;
-    let average = 0;
-    
-    for (let grade of grades) {
-        result += grade;
-        index++;
+
+    for (let value of array) {
+        result += value;
+        index++
     }
 
-    average = result / index;
-
-    if (average == 100 || average >= 90) {
-        return "A";
-    } else if (average == 89 || average >= 80) {
-        return "B";
-    } else if (average == 79 || average >= 70) {
-        return "C";
-    } else if (average == 69 || average >= 60) {
-        return "D";
-    } else if (average == 59 || average >= 50) {
-        return "C";
-    } else {
-        return "Invalid";
-    }
+    return result / index;
 }
