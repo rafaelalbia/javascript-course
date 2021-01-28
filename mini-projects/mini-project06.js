ea02cba5891058c930823e7f18ec3385fa655f86
rafaelalbia <rafaelalbia@outlook.com>
2021-01-28 11:45:36 -0300

    Mini-Project06 import

---
+++ mini-projects/mini-project06.js
@@ -0,0 +1,15 @@
+const movie = {
+    title: "Godzilla vs King Kong",
+    year: 2021,
+    style: "Scientific Fiction"
+}
+
+showProperties(movie);
+
+function showProperties(object) {
+    for (let index in object) {
+        if (typeof object[index] == "string") {
+            console.log(object[index]);
+        }
+    }
+}
