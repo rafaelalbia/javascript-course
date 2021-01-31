e9d5e79c7eefd6fb0d1abeedbc3436594812e755
rafaelalbia <rafaelalbia@outlook.com>
2021-01-31 01:31:13 -0300

    Exercise import

--- exercises/exercise13.js
+++ exercises/exercise13.js
@@ -1,4 +1,4 @@
-const number = [1, 2, 3];
+const number = [1, 2, 3, 1, 4];
 
 // Start
 number.unshift(0);
@@ -11,3 +11,8 @@ console.log(number);
 // Final
 number.push(5);
 console.log(number);
+
+// Find a value
+console.log(number.indexOf(2));
+console.log(number.lastIndexOf(1));
+console.log(number.includes("a"));
