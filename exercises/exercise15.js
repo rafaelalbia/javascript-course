0bd6a10daae27c22240d80ff483aaac2cfb0e87f
rafaelalbia <rafaelalbia@outlook.com>
2021-01-31 01:51:08 -0300

    Exercise import

---
+++ exercises/exercise15.js
@@ -0,0 +1,10 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+const last = numbers.pop();
+console.log(last);
+
+const first = numbers.shift();
+console.log(first);
+
+const middle = numbers.splice(3, 1);
+console.log(middle)
